5b5611c3cff24af52ef5075a657a514c27c5c6b9
Marcus <macke_125@hotmail.com>
2020-01-09 11:13:01 +0100

    Skapat Book klassen. Med getters och setter och toString(). Marcus och Calle

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Book bok = new Book("123", "hej", "hehe", (byte) 2, "2323", "efeef");
+
+
+        System.out.println(bok.toString());
+        bok.setTitle("test");
+
+        System.out.println(bok.toString());
 
     }
 }
