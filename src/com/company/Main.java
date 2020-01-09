33de7be1dbd25be0463fa5518cfd631c5cd1a6c1
Carljohan-hornestam <carlis.hornestam@hotmail.com>
2020-01-09 12:27:57 +0100

    Skapat Library klassen och några småmetoder

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-
+        Library library = new Library();
+        library.identification();
     }
 }
