85e7b3444f6bf54954318012df6980b8e4a03a24
Magnus <isthisthereallife@riseup.net>
2020-01-09 11:27:46 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
-        Book bok = new Book("123", "hej", "hehe", (byte) 2, "2323", "efeef");
-
-
-        System.out.println(bok.toString());
-        bok.setTitle("test");
-
-        System.out.println(bok.toString());
+        System.out.println("Hello World!");
 
     }
 }
