4e4312fcac94ed1900f6b34b34dbb5b7360f92cf
Hang <HangTran@hotmail.se>
2020-01-13 10:54:12 +0100

    Hang&Marcus
    AddBook Feature

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,9 +7,6 @@ import java.nio.file.Paths;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        User h = new User("HA","gatan 3","mailen@mail.com","0802730");
-        System.out.println(h.getId());
-        Library library = new Library();
-        library.register();
+
     }
 }
