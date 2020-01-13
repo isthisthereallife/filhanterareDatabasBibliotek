67bbc54070874b4e9052ced8ac2db36d71052a21
Magnus <isthisthereallife@riseup.net>
2020-01-13 11:42:24 +0100

    switch med sträng istället för int

--- src/com/company/Library.java
+++ src/com/company/Library.java
@@ -5,28 +5,35 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Library{
     Scanner scan = new Scanner(System.in);
     private ArrayList<User> users = new ArrayList<>();
     private ArrayList<Book> books;
+    private User activeUser;
 
     public Library(){
-    final Path path = Paths.get("books.txt");
     books = new ArrayList<Book>();
-    }
+    initializeBookAndUserObjects();
 
+
+
+    }
+    private void initializeBookAndUserObjects(){
+        //TODO initiera bok-objekten och user-objekten här
+    }
     public void identification(){
         System.out.println("Who would you like to login as?");
         System.out.println("1: Admin");
         System.out.println("2: User");
-        byte choice = scan.nextByte();
+        String choice = scan.nextLine();
 
-        if (choice == 1){
+        if (choice.equals("1")){
             adminMenu();
         }
-        if (choice == 2){
+        else if (choice.equals("2")){
             userLoginMenu();
         }
     }
@@ -36,8 +43,8 @@ public class Library{
         System.out.println("1: Add book to library");
         System.out.println("2: Remove book from library");
         System.out.println("3: Edit book in library");
-        byte choice = scan.nextByte();
-        if (choice == 1){
+        String choice = scan.nextLine();
+        if (choice.equals("1")){
             addBook();
         }
     }
@@ -52,39 +59,118 @@ public class Library{
         }
     }
     public void loginMenu(){
+        boolean loggedIn = false;
         System.out.println("What is your login id? ");
         String loginId = scan.nextLine();
         for (User user : users){
-            String id = user.getId();
 
-            if (id.equals(loginId)){
+            if (user.getId().equals(loginId)){
                 System.out.println("Login successful!");
+                activeUser = user;
+                loggedIn = true;
                 userMenu();
+
                 break;
             }
         }
-        System.out.println("Login failed.");
+        if (!loggedIn)
+            System.out.println("Login failed.");
     }
 
     private void userMenu() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome "+activeUser.getName()+ "\nPlease choose operation:");
+        System.out.println();
+
+        boolean running = true;
+        String number = "0";
+        do {
+            System.out.println("1: View available books");
+            System.out.println("2: Borrow a book");
+            System.out.println("3: Return a book");
+            System.out.println("4: View all books in the library");
+            if (!this.activeUser.activeLoansInfo().equals("")){
+                System.out.println("5: View active loans");
+            }
+            System.out.println("0: Exit");
+
+            try {
+                number = scan.nextLine();
+            }catch(InputMismatchException e){
+                userMenu();
+            }
+            switch (number) {
+                case "1": {
+                    //TODO printa alla böcker med status AVAILABLE
+                    running = rerunPrompt();
+                    break;
+                }
+                case "2": {
+                    //TODO låna en bok
+                    running = rerunPrompt();
+                    break;
+                }
+                case "3": {
+                    //TODO lämna tillbaka en bok
+                    rerunPrompt();
+                    break;
+                }
+                case "4": {
+                    //TODO printa ALLA böcker
+                    running = rerunPrompt();
+                    break;
+                }
+                case "5": {
+                    System.out.println(activeUser.activeLoansInfo());
+                    running = rerunPrompt();
+                    break;
+                }
+                case "0":
+                    running = false;
+                    break;
+                default:
+
+            }
+        } while (running);
     }
+
+
+
+
+    private boolean rerunPrompt(){
+        int choice = 0;
+        do {
+            System.out.println("\n1. Back to main menu");
+            System.out.println("2. Quit");
+            try {
+                choice = new java.util.Scanner(System.in).nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter 1 or 2.");
+            }
+            if(choice == 1) return true;
+            if(choice == 2) return false;
+        }while (true);
+    }
+
 
     public void register() {
         Scanner scan = new Scanner(System.in);
         System.out.println("Your name:");
         String name = scan.nextLine();
-        System.out.println("Your adress:");
-        String adress = scan.nextLine();
+        System.out.println("Your address:");
+        String address = scan.nextLine();
         System.out.println("Your mail: ");
         String mail = scan.nextLine();
         System.out.println("Telephone number:");
         String tel = scan.nextLine();
-        users.add(new User(name, adress, mail, tel));
+
+        users.add(new User(name, address, mail, tel));
+
         int i = users.size() - 1;
         User user = users.get(i);
         String uniqueId = user.getId();
         String fileName = "database/users/" + uniqueId;
-        String str = String.format("%s\n%s\n%s\n%s\n%s", uniqueId, name, adress, mail, tel);
+        String str = String.format("uniqueId: %s\nname: %s\naddress: %s\nmail: %s\ntel: %s", uniqueId, name, address, mail, tel);
         Base b = new Base ();
         b.writeToFile(fileName, str);
         System.out.printf("Registration completed!\nYour unique id is: %s\n", uniqueId);
@@ -117,7 +203,7 @@ public class Library{
 
         books.add(new Book(title, author, genre, year, isbn));
         String fileName = "database/books/" + isbn;
-        String str = String.format("%s\n%s\n%s\n%s\n%s", isbn, title, author, year, genre);
+        String str = String.format("isbn: %s\ntitle: %s\nauthor: %s\nyear: %s\ngenre: %s", isbn, title, author, year, genre);
         Base b = new Base ();
         b.writeToFile(fileName, str);
     }
