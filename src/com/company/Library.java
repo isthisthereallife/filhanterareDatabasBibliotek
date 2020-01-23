20d483f8c40cdabc23276f7255555bb0c064803a
Alexander Persson <55090569+miniharald@users.noreply.github.com>
2020-01-23 15:37:07 +0100

    deletebook with author, not done yet

--- src/com/company/Library.java
+++ src/com/company/Library.java
@@ -575,14 +575,20 @@ public class Library {
     }
 
     public void deleteBook(Book aBook) {
-
+        String authorsBooks = "";
         Path path = Paths.get("database/books/" + aBook.getId() + ".txt");
         books.removeIf(book -> book.getIsbn().equals(aBook.getIsbn()));
         for (Author author : authors) {
-            for(Book book : author.getBibliography()){
-                if
+            for(Book book : author.getBibliography()) {
+                if (book.getIsbn().equals(aBook.getIsbn())) {
+                    author.removeFromBibliography(book);
+                    for (book : author.getBibliography()) {
+                        authorsBooks = authorsBooks.concat(book.getIsbn() + " ");
+                    }
+                    author.editFile("database/authors/" + author.getAuthorId() + ".txt", "bibliography", "bibliography: " + 2);
+                }
             }
-        }
+            }
 
         aBook.deleteFiles(path);
         System.out.println(aBook.getTitle() + " is now deleted.");
